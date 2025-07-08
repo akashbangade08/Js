47e66a10d1a050f475ddc5f5e0dc7638e5d85247
Akash Bangade <103981697+akashbangade08@users.noreply.github.com>
2025-07-08 03:51:41 +0000

    Math and num

---
+++ 01_basics/07_number_and_maths.js
@@ -0,0 +1,27 @@
+const number = 20;
+
+const balance = new Number(100);
+// console.log(balance.toString());
+// console.log(balance.toFixed(1));
+
+const newNum = 128.23
+// console.log(newNum.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/*****************MATHS*******************/
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.4));
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(5.9));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random()*(max - min +1)+ min));
